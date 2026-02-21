9777603c2734e708c4d26d27b6119052e78f939a
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-02-22 01:09:03 +0530

    yet to fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useMemo } from 'react';
+// import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
@@ -9,7 +9,7 @@ import {
     WalletMultiButton,
     WalletConnectButton
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+// import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -17,14 +17,14 @@ import { Airdrop } from './components/Airdrop';
 
 function App() {
 
-  const network = WalletAdapterNetwork.Devnet;
+  // const network = WalletAdapterNetwork.Devnet;
 
-    // You can also provide a custom RPC endpoint.
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  //   // You can also provide a custom RPC endpoint.
+  //   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   return (
     <>
-       <ConnectionProvider endpoint={endpoint}>
+       <ConnectionProvider config={{commitment: "confirmed"}} endpoint={"https://api.devnet.solana.com"}>
             <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
                     <div className='flex flex-col items-center justify-center gap-3 pt-10'>
--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -1,11 +1,42 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useState } from "react";
 
 export function Airdrop() {
+    // const [value , setValue] = useState(0)
+    const wallet = useWallet();
+    const {connection} = useConnection();
+    
+    async function sendAirdrop() {
+        if (!wallet.publicKey)
+            return;
+        try {
+            const amount = (document.getElementById("amountsol") as HTMLInputElement | null)?.value;
+            const lamports = Math.floor(Number(amount) * 1000000000);
+            if (!amount) {
+                alert("Please enter an amount");
+                return;
+            }
+            const signature = await connection.requestAirdrop(wallet.publicKey, lamports)
+            const latestBlockHash = await connection.getLatestBlockhash();
+        await connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: signature,
+        });
+            alert(`Airdroped ${amount} SOL to ${wallet.publicKey.toString()}`)
+        }catch (err) {
+            alert("Airdrop failed: " + err)
+        }
+    }
+
     return (
         <div className="flex flex-col items-center pt-10">
+            <p className="uppercase font-bold text-green-700 mb-2 border p-2 rounded-lg">wallet publickey</p>
+            <p>{wallet.publicKey ? wallet.publicKey.toString() : "no wallet connected"}</p>
             <p>Airdrop yourself some SOL to get started!</p>
             <div className="flex justify-center pt-5">
-               <input className="bg-white border border-gray-300 rounded-l-lg p-2" type="text" placeholder="Amount" />
-               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-r-lg">Airdrop</button>
+               <input id="amountsol" className="bg-white border border-gray-300 rounded-l-lg p-2" type="text" placeholder="Amount" />
+               <button onClick={sendAirdrop} className="hover:cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-r-lg">Airdrop</button>
             </div>
             
         </div>
