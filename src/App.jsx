a10120a383064409c789adbb85763421a023393f
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-02-21 22:43:24 +0530

    added providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import './App.css'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+// import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
 
   return (
     <>
-      <div className='flex justify-center'>
-
-      </div>
+       <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+            <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                    {/* <WalletMultiButton />
+                    <WalletDisconnectButton />
+                      */}
+                    <div>
+                      
+                    </div>
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
     </>
   )
 }
